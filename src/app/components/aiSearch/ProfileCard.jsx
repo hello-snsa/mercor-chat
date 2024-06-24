a0fcba6a80f932a5782262e2532114a23e4190e3
ali.shahanshah <ali.shahanshah@ril.com>
2024-06-24 23:55:31 +0530

    awards and projects added

--- src/app/components/aiSearch/ProfileCard.jsx
+++ src/app/components/aiSearch/ProfileCard.jsx
@@ -3,33 +3,34 @@ import React, { useEffect } from 'react'
 import { USER_VIDEO } from '../../../utils/apiConstants';
 
 export default function ProfileCard({ candidateData }) {
-    const { fullTime, partTime, fullTimePrice, fullTimeAvailability, partTimeAvailability, partTimePrice,videoRecording } = candidateData;
+    const { fullTime, partTime, fullTimePrice, fullTimeAvailability, partTimeAvailability, partTimePrice, videoRecording,projects,awards,education,workExperience } = candidateData;
 
     const [recordingUrl, setRecordingUrl] = React.useState('')
 
-    const getVideoRecording = async() => {
+    const getVideoRecording = async () => {
         try {
             const response = await axios.post(USER_VIDEO,
                 {
-                    "bucketName":"ai_interviewer_recordings","objectName":videoRecording.split("/ai_interviewer_recordings/")[1]
-            },{
-                headers:{
-                    'Content-Type':'application/json'
+                    "bucketName": "ai_interviewer_recordings", "objectName": videoRecording.split("/ai_interviewer_recordings/")[1]
+                }, {
+                headers: {
+                    'Content-Type': 'application/json'
                 },
-                body:JSON.stringify({videoRecording})
+                body: JSON.stringify({ videoRecording })
             })
-            const data = await response.data;
-            setRecordingUrl(data.url)
+            const data = await response?.data;
+            setRecordingUrl(data?.url)
         } catch (error) {
             console.log(error)
         }
     }
+
     useEffect(() => {
-        recordingUrl==="" && getVideoRecording()
+        recordingUrl === "" && getVideoRecording()
     }, [])
+
     return (
         <div className='profileCard' >
-            {/* Toggle button */}
             <div className='flex  gap-1 flex-space-between' style={{ width: "100rem", "margin": "1rem" }}>
                 <>
                     {fullTime ? <div>
@@ -46,21 +47,39 @@ export default function ProfileCard({ candidateData }) {
                     </div> : null}
                 </>
 
-
-
             </div>
             <div>
                 <p>AI Interview</p>
 
-               {recordingUrl!=="" ? <video width="750" height="500" controls >
+                {recordingUrl !== "" ? <video width="750" height="500" controls >
                     <source src={recordingUrl} type="video/mp4" />
-                </video>:null}
+                </video> : null}
 
             </div>
             {/* Work experience */}
-            <div>
-                        <h2>Work Experience</h2>
-            </div>
+            {workExperience?.length>0?<div>
+                <h2>Work Experience</h2>
+
+            </div>:null}
+            {/* Education */}
+            {education?.length>0 ?<div>
+                <h2>Education</h2>
+            </div>:null}
+            {/* Awards */}
+            {awards?.length>0? <div>
+                <h2>Awards</h2>
+                {awards?.map((award, index) => (
+                        <li key={index} className='profileCard_award'>{award?.description}</li>
+                ))}
+            </div>:null}
+            {/* Projects */}
+            {projects?.length>0 ?<div>
+                <h2>Projects</h2>
+                {projects?.map((project, index) => (
+                        <li key={index} className='profileCard_project'>{project?.title} - {project?.description}</li>
+                ))}
+
+            </div>:null}
         </div>
     )
 }
