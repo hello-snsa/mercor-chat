748d92932e6853b70c93274eea3a778960695be6
ali.shahanshah <ali.shahanshah@ril.com>
2024-06-25 17:59:17 +0530

    profile card all data added

--- src/app/components/aiSearch/ProfileCard.jsx
+++ src/app/components/aiSearch/ProfileCard.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 import { USER_VIDEO } from '../../../utils/apiConstants';
 
 export default function ProfileCard({ candidateData }) {
-    const { fullTime, partTime, fullTimePrice, fullTimeAvailability, partTimeAvailability, partTimePrice, videoRecording,projects,awards,education,workExperience } = candidateData;
+    const { fullTime, partTime, fullTimePrice, fullTimeAvailability, partTimeAvailability, partTimePrice, videoRecording, projects, awards, education, workExperience } = candidateData;
 
     const [recordingUrl, setRecordingUrl] = React.useState('')
 
@@ -57,29 +57,68 @@ export default function ProfileCard({ candidateData }) {
 
             </div>
             {/* Work experience */}
-            {workExperience?.length>0?<div>
+            {workExperience?.length > 0 ? <div>
                 <h2>Work Experience</h2>
-
-            </div>:null}
+                {
+                    workExperience?.map((work, index) => (
+                        <>
+                            {work?.company ? <div key={index} className='profileCard_education flex flex-space-between'>
+                                <div className='flex'>
+                                    <img src={work?.companyLogo} alt='company Logo' className='round-icon' />
+                                    <div>
+                                        <p>{work?.role}</p>
+                                        <p>{work?.company}{work?.company && work?.locationCity ? ", " : null}{work?.locationCity}{
+                                            (work?.company || work?.locationCity) && work?.locationCountry ? ", " : null}{work?.locationCountry}</p>
+                                            <p>{work?.description}</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <span>{work?.startDate}</span>
+                                    {work?.startDate ? " - " : null}
+                                    <span>{work?.endDate ?? "Present"}</span>
+                                </div>
+                            </div> : null}
+                        </>))
+                }
+            </div> : null}
             {/* Education */}
-            {education?.length>0 ?<div>
+            {education?.length > 0 ? <div>
                 <h2>Education</h2>
-            </div>:null}
+                {
+                    education?.map((edu, index) => (
+                        <>
+                            {edu?.degree ? <div key={index} className='profileCard_education flex flex-space-between'>
+                                <div className='flex'>
+                                    <img src={edu?.schoolLogo} alt='education icon' className='round-icon' />
+                                    <div>
+                                        <p>{edu?.degree}{edu?.degree && edu?.major ? ", " : null}{edu?.major}</p>
+                                        <p>{edu?.school}</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <span>{edu?.startDate}</span>
+                                    {edu?.startDate && edu?.endDate ? " - " : null}
+                                    <span>{edu?.endDate}</span>
+                                </div>
+                            </div> : null}
+                        </>))
+                }
+            </div> : null}
             {/* Awards */}
-            {awards?.length>0? <div>
+            {awards?.length > 0 ? <div>
                 <h2>Awards</h2>
                 {awards?.map((award, index) => (
-                        <li key={index} className='profileCard_award'>{award?.description}</li>
+                    <li key={index} className='profileCard_award'>{award?.description}</li>
                 ))}
-            </div>:null}
+            </div> : null}
             {/* Projects */}
-            {projects?.length>0 ?<div>
+            {projects?.length > 0 ? <div>
                 <h2>Projects</h2>
                 {projects?.map((project, index) => (
-                        <li key={index} className='profileCard_project'>{project?.title} - {project?.description}</li>
+                    <li key={index} className='profileCard_project'>{project?.title} - {project?.description}</li>
                 ))}
 
-            </div>:null}
+            </div> : null}
         </div>
     )
 }
