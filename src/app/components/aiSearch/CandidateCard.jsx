bcd10126b96a0e42c098c43811312163f6102224
ali.shahanshah <ali.shahanshah@ril.com>
2024-06-27 14:27:44 +0530

    candidate card css web done

--- src/app/components/aiSearch/CandidateCard.jsx
+++ src/app/components/aiSearch/CandidateCard.jsx
@@ -1,13 +1,15 @@
-import PropTypes from 'prop-types'
-import { BookmarkActive, BookmarkWhite, HireIcon } from '../../../assets/images';
 import { useState } from 'react';
+import PropTypes from 'prop-types'
+
 import ProfileCard from './ProfileCard';
+import { BookmarkActive, BookmarkWhite, HireIcon } from '../../../assets/images';
+import { t } from 'i18next';
 
 export default function CandidateCard({ candidateData }) {
 
-    const { profilePic,finalImageUrl, name, skills, country, yearsOfWorkExperience, summary, fullTime, partTime,status } = candidateData;
+    const { finalImageUrl, name, skills, country, yearsOfWorkExperience, summary, fullTime, partTime, status, fullTimePrice, partTimePrice } = candidateData;
 
-    const [isShortlisted, setIsShortlisted] = useState(status==="shortlisted"?true:false);
+    const [isShortlisted, setIsShortlisted] = useState(status === "shortlisted" ? true : false);
     const [toggleView, setToggleView] = useState(false);
 
     const handleShortlist = () => {
@@ -18,9 +20,9 @@ export default function CandidateCard({ candidateData }) {
     const handleToggleView = () => {
         setToggleView(!toggleView);
     }
+
     return (
         <div className='chatCanvas_message_candidate'>
-            {/* Top card */}
             <div>
                 <div className='chatCanvas_message_candidate_top'>
                     <div className='chatCanvas_message_candidate_topLeft'>
@@ -30,49 +32,44 @@ export default function CandidateCard({ candidateData }) {
 
                         <span className='chatCanvas_message_candidate_name'>{name}</span>
                         <span>{" | "}</span>
-                        <span className='chatCanvas_message_candidate_skills'>Exp: {yearsOfWorkExperience} Years</span>
+                        <span className='chatCanvas_message_candidate_exp'>Exp: {yearsOfWorkExperience} Years</span>
                         <span>{" | "}</span>
                         <span className='chatCanvas_message_candidate_country'>{country}</span>
 
                     </div>
-                    <div className='flex flex-center gap-1'>
-                        <div>
-                            <button className='btn-primary btn-shadow flex flex-center gap-1' style={{
-                                color: isShortlisted ?
-                                    "#4A00E0" : "white", backgroundColor: isShortlisted ? "#CED2D8" : "#6E49BD"
-                            }} onClick={handleShortlist}>
-                                <img src={isShortlisted ? BookmarkActive : BookmarkWhite} alt='Shortlisted' />
-                                {isShortlisted ? "Shortlisted" : "Shortlist"}
-                            </button>
-                        </div>
-                        <div>
-                            <button className='btn-primary btn-shadow flex flex-center' style={{ height: "20px" }}>
-                                <img src={HireIcon} alt='Hire' className='hireIcon' />
-                                <p className=' ' >Hire Instantly</p> </button>
-                        </div>
+                    <div className='chatCanvas_message_candidate_topRight'>
+                        <button className={`btn-primary btn-shadow flex flex-center gap-1 ${isShortlisted ? "btn-active" : "btn-inactive"}`} onClick={handleShortlist}>
+                            <img src={isShortlisted ? BookmarkActive : BookmarkWhite} alt='Shortlisted' />
+                            {isShortlisted ? t("tShortlisted") : t("tShortlist")}
+                        </button>
+                        <button className='btn-primary btn-shadow flex flex-center btn-hire' >
+                            <img src={HireIcon} alt='Hire' className='hireIcon' />
+                            <p className=' ' >Hire Instantly</p> </button>
                     </div>
                 </div>
-                <div className='flex gap-1'>
-                    <span>Available for :</span>
-                    <span>{fullTime ? "Full Time" : null}</span>
-                    <span>{partTime ? "Part Time" : null}</span>
-                </div>
-                <div className='chatCanvas_message_candidate_skills flex gap-1'>
-                    {"Expert In : "} {skills.map((skill, index) => (
-                        <div key={index} className='chatCanvas_message_candidate_skill'>{skill}</div>
-                    ))}
-                </div>
-                <div>
-                    <p>{summary}</p>
+                <div className=' chatCanvas_message_candidate_middle'>
+                    <div className='chatCanvas_message_candidate_info'>
+                        <span><b>Available for :</b></span>
+                        <span>{fullTime ? `Full Time @ $ ${fullTimePrice}` : null}</span>
+                        <span>{partTime ? `| Part Time $ ${partTimePrice}` : null}</span>
+                    </div>
+                    <div className='chatCanvas_message_candidate_info'>
+                        <b>{t("expertIn")}</b> {skills.map((skill, index) => (
+                            <p key={index} >{skill}</p>
+                        ))}
+                    </div>
+                    <div className='chatCanvas_message_candidate_info'>
+                        <p className='bg-dark max-w-60'><i>{summary}</i></p>
+                    </div>
                 </div>
             </div>
             {/* Bottom card */}
             <div>
-               {toggleView && <ProfileCard candidateData={candidateData} />}
+                {toggleView && <ProfileCard candidateData={candidateData} />}
             </div>
             {/* Card Toggler */}
             <div>
-                <button className='btn-primary btn-extended' onClick={handleToggleView}>View Profile</button>
+                <button className='btn-primary btn-extended' onClick={handleToggleView}>{toggleView ? "Hide details ^" : "View Profile"}</button>
             </div>
         </div>
     )
@@ -84,7 +81,11 @@ CandidateCard.propTypes = {
         name: PropTypes.string.isRequired,
         skills: PropTypes.arrayOf(PropTypes.string).isRequired,
         country: PropTypes.string.isRequired,
-        yearsOfWorkExperience: PropTypes.number.isRequired
+        yearsOfWorkExperience: PropTypes.number.isRequired,
+        summary: PropTypes.string.isRequired,
+        fullTime: PropTypes.bool.isRequired,
+        partTime: PropTypes.bool.isRequired,
+        status: PropTypes.string.isRequired
     })
 }
 
