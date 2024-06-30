58d8341939b93a6669a3aa452a3e477724096fbc
ali.shahanshah <ali.shahanshah@ril.com>
2024-07-01 00:38:22 +0530

    homepage responsive css added

--- src/app/components/homepage/HomepageContainer.jsx
+++ src/app/components/homepage/HomepageContainer.jsx
@@ -5,89 +5,68 @@ import { SearchPageSS, LoginSS, SearchBarSS } from '../../../assets/images';
 
 export default function HomepageContainer() {
   return (
-    <div className="homepage-body">
-        <div className="homepage-topSection">
-          <span className="homepage-headingText">
-            {t('homepageHeadingText')}
-          </span>
-          <span>
-            {t('homepageSubHeadingText')}
-            <br />
-            It uses advanced algorithms to get you your desired results.
-          </span>
-          <Link to="/ai-search" >
-            <button className="try-for-free-btn btn-primary">Try for free</button>
-          </Link>
-          <div>
-
-            <img className="Ai-search page screenshot" src={SearchPageSS} style={{ width: "60%", border: "1px solid white", borderRadius: "20px" }} />
-
-          </div>
+    <div className="homepage_body">
+      <div className="homepage_topSection">
+        <p className="homepage_headingText">
+          {t('homepageHeadingText')}
+        </p>
+        <div className='homepage_subHeading'>
+          <p>{t('homepageSubHeadingText')}</p>
+          <p>{t('homepageSubHeadingText2')}</p>
         </div>
-        <div className="who-uses-section">
-          <a className="font-16 font-weight-600 page-title">WHO USES MARCUS</a>
-          <span className="sub-heading">Built For Everyone</span>
-          <div className="who-uses-skills">
-            <span>React</span>
-            <span>Java</span>
-            <span>PHP</span>
-            <span>CSS</span>
-            <span>Vloggers</span>
-            <span>Marketing</span>
-            <span>SEO-Specialist</span>
-          </div>
-          <Link to="/ai-search" >
-            <button className="try-for-free-btn btn-primary">Try for free</button>
-          </Link>
+        <Link to="/ai-search" >
+          <button className="login-btn btn-primary">{t('tTryForFree')}</button>
+        </Link>
+        <div>
+          <img className="homepage_homess" src={SearchPageSS} />
         </div>
-
-        <div className="how-works-section">
-          <div className="colum-flex-gap-0">
-            <a className="font-16 font-weight-600 page-title">HOW IT WORKS</a>
-            <span className="sub-heading">Simple yet smart</span>
-          </div>
-
-          <div className="how-it-works">
-            <div>
-            </div>
-            <div className="step-1">
-              <span className="font-16 font-weight-600 page-title">STEP 1</span>
-              <span className="font-16 font-weight-600">
-                Log in To Your Account
-              </span>
-              <span>{t('step-1-1')}</span>
-              <span>{t('step-1-2')}</span>
-              <span>{t('step-1-3')}</span>
-            </div>
-
-            <img className="form-image" src={LoginSS} style={{ width: "40%" }} />
-          </div>
-
-          <div className="how-it-works">
-
-            <div>
-              {/* <div className="circle"></div> */}
-              {/* <div className="circle-1"></div> */}
-
-
-            </div>
-
-            <div className="step-1">
-              <span className="font-16 font-weight-600 page-title">STEP 2</span>
-              <span className="font-16 font-weight-600">
-                Go to AI-Search page
-              </span>
-              <span>{t('step-2-1')}</span>
-              <span>{t('step-2-2')}</span>
-              <span>{t('step-2-3')}</span>
-            </div>
-
-            <img className="form-image img-searchbar" src={SearchBarSS} />
-          </div>
-
-
-        </div>
-        <div className="bottom-section"></div>
       </div>
+      <div className="homepage_whoUsesSection">
+        <p className="homepage_pageTitle">{t('tWhoUsesMarcus')}</p>
+        <span className="homepage_subHeading">{t("tBuiltForEveryone")}</span>
+        <div className="homepage_whoUsesSkills">
+          <span>React</span>
+          <span>Java</span>
+          <span>PHP</span>
+          <span>CSS</span>
+          <span>Vloggers</span>
+          <span>Marketing</span>
+          <span>SEO-Specialist</span>
+        </div>
+        <Link to="/ai-search" >
+          <button className="login-btn btn-primary">{t('tTryForFree')}</button>
+        </Link>
+      </div>
+      <div className="homepage_howWorksSection">
+        <div className="homepage_pageTitle">
+          <p>{t('tHowItWorks')}</p>
+          <span className="homepage_subHeading">{t('tSimpleYetSmart')}</span>
+        </div>
+        <div className="homepage_howItWorks">
+          <div className="homepage_step">
+            <p className="homepage_stepTitle">{t('tStep1')}</p>
+            <div className='homepage_stepsInstruction'>
+              <p>{t('tLogInToYourAccount')}</p>
+              <p>{t('step-1-1')}</p>
+              <p>{t('step-1-2')}</p>
+              <p>{t('step-1-3')}</p>
+            </div>
+          </div>
+          <img className="homepage_screenShot homepage_loginss" src={LoginSS} />
+        </div>
+        <div className="homepage_howItWorks">
+          <div className="homepage_step">
+            <p className="homepage_stepTitle">{t('tStep2')}</p>
+            <div className='homepage_stepsInstruction'>
+              <p>{t('tgoToAiSearchPage')}</p>
+              <p>{t('step-2-1')}</p>
+              <p>{t('step-2-2')}</p>
+              <p>{t('step-2-3')}</p>
+            </div>
+          </div>
+          <img className="homepage_screenShot homepage_searchbar" src={SearchBarSS} />
+        </div>
+      </div>
+    </div>
   )
 }
